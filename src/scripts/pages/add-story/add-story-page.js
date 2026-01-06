b6e912ae538fe0e68e2e9cebb874433def9f7c51
Andakaraa <wardonoandakara@gmail.com>
2026-01-06 17:35:59 +0700

    Refactor AddStoryPage to use local marker images

--- src/scripts/pages/add-story/add-story-page.js
+++ src/scripts/pages/add-story/add-story-page.js
@@ -3,11 +3,15 @@ import 'leaflet/dist/leaflet.css';
 import storyAPI from '../../data/story-api';
 import authAPI from '../../data/auth-api';
 import indexedDBHelper from '../../utils/indexeddb-helper';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
 });
 
 class AddStoryPresenter {
